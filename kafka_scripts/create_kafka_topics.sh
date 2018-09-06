bd1bb0d5e472884455235eed921c24fe7562ecb0
Luis Figueroa <lfigueroa@HW13858-2.local>
2018-09-06 11:34:31 -0400

    Added kafka topics creation for each data source shell script

---
+++ kafka_scripts/create_kafka_topics.sh
@@ -0,0 +1,5 @@
+/usr/hdp/3.0.0.0-1634/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic ssh
+/usr/hdp/3.0.0.0-1634/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic dhcp
+/usr/hdp/3.0.0.0-1634/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic dns
+/usr/hdp/3.0.0.0-1634/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic ftp
+/usr/hdp/3.0.0.0-1634/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic http
